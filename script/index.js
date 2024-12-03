e07a901ef3e952b61e4eed8d3cd9025588220be9
Debugger <mbiydzenyuyeileen@gmail.com>
2024-12-03 06:43:04 +0100

    fixed linter errors on html document

--- script/index.js
+++ script/index.js
@@ -1,37 +1,5 @@
-// Initialization of Variables
-// an Array of denominations, result and initialise i=0
-// loop through tha Array containing the notes
-// divide each element by the amount
-// take the remender and keep looping till amount = 0
-// return result
-// print
-
-// funtion def
-function calculateBanknotesWhile (amount) {
-  // initialisation of variables
-  const denominations = [10000, 5000, 2000, 1000, 500, 100]
-  const result = {}
-  let i = 0
-  // while 23700 > 0 and i < legth of array loop
-  while (amount > 0 && i < denominations.length) {
-    const denom = denominations[i]
-    if (amount >= denom) {
-      const count = Math.floor (amount / denom) // divides
-      result[denom] = count
-      amount %= denom // get the remainder
-    }
-    i++ // keep looping
-  }
-  return result
-}
-// function call
-const amount = 23700
-const output = calculateBanknotesWhile(amount)
-console.log(output)
-
 // Write a function that takes a sentence as input and returns the longest word in the sentence
 // If there are multiple words of the same length, return the first one
-
 // create a function
 // split the sentence in to individual words
 // initialize a variable to keep track of the longest word and its length
@@ -43,18 +11,38 @@ console.log(output)
 const findLongestWord = (sentence) => {
   const words = sentence.split('')
   let longestWord = ''
-
   for (let i = 0; i < words.length; i++) { 
     if (words[i].length > longestWord.length) {
       longestWord = words[i]
     }
   }
-
   return longestWord  
 }
 const sentence = findLongestWord('The quick brown fox jumps over the lazy dog')
 console.log(sentence)
 
+// Initialization of Variables
+// an Array of denominations, result and initialise i=0
+// loop through tha Array containing the notes
+// divide each element by the amount
+// take the remender and keep looping till amount = 0
+// return result
+// print
+function calculateBanknotesWhile(amount) {
+  const denominations = [10000, 5000, 2000, 1000, 500, 100]
+  const result = {}
+  let i = 0
+  while (amount > 0 && i < denominations.length) {
+    const denom = denominations[i]
+    if (amount >= denom) {
+      const count = Math.floor(amount / denom)
+      result[denom] = count
+      amount %= denom
+    }
+    i++
+  }
+  return result
+}
 
 // Bank Transaction Tracker
 // Write a function that takes an initial balance and a list of transactions 
@@ -67,13 +55,11 @@ console.log(sentence)
 // update the finalBalnce without redeclaring it
 // RETURN finaBalance
 // consolelog function
-
-function calculateFinalBalance (initialBalance, transactions){
-  let finalBalance = initialBalance
+function calculateFinalBalance(initialBalance, transactions) {
+  let finalBalance = initialBalance;
   for (let i = 0; i < transactions.length; i++) {
     finalBalance += transactions[i]
   }
-  return finalBalance
+  return finalBalance;
 }
-
-console.log (calculateFinalBalance( 10000, [3000, -2000, -1500, 5000]))
+console.log(calculateFinalBalance(10000, [3000, -2000, -1500, 5000]))
